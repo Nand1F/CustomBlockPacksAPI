e914900427bb524f7f46b100306f800b2c661525
Зирнзак Фердинанд <zirnzakf@gmail.com>
2025-08-12 19:36:41 +0200

    first commit

---
+++ src/main/kotlin/com/cbpapi/CustomBlockPacksAPI.kt
@@ -0,0 +1,15 @@
+package com.cbpapi
+
+import net.fabricmc.api.ModInitializer
+import org.slf4j.LoggerFactory
+
+object CustomBlockPacksAPI : ModInitializer {
+    private val logger = LoggerFactory.getLogger("customblockpacksapi")
+
+	override fun onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		logger.info("Hello Fabric world!")
+	}
+}
